428f96c89440e8b5cb3d0678e9b567a5310793d3
Rahil Charniya <rahilcharniya@Rahils-MBP-2.attlocal.net>
2024-11-02 19:46:29 -0500

    trial

--- conditional assignment.py
+++ conditional assignment.py
@@ -1,3 +1,11 @@
-post = "aa"
-if "sign" :
-    print("be ware")
+#Task 1
+number = input("Enter a number: ")
+
+if number > 0:
+    print("The number is positive.")
+elif number == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
+
+#Task 2
